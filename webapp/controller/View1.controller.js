95c6cfccbdccdc8a5a068a153c9afbb06aab2a59
MrBass <belbizri@projexia.ca>
2020-05-30 01:57:07 -0400

    adjusting ids

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -7,6 +7,24 @@ sap.ui.define([
 	return Controller.extend("com.Innovation.ui5Project.controller.View1", {
 		onInit: function () {
 			 
-		}
+		},
+		onListPlanningCalendarDrop: function(oEvent) {
+			var oDroppedControl = oEvent.getParameter("droppedControl");
+			var oDragSession = oEvent.getParameter("dragSession");
+			var cliId = oDroppedControl.getId();
+			var rowId = cliId.replace("-CLI", "");
+			var pcRow = sap.ui.getCore().byId(rowId);
+			var oBindingContext = pcRow.getBindingContext("calendarModel");
+			var resourceObj = oBindingContext.getObject();
+			var oDraggedRowContext = oDragSession.getComplexData("onListDragContext");
+		},
+	
+		onListPlanningCalendardragStart: function(oEvent) {
+			var oDragSession = oEvent.getParameter("dragSession");
+			var oDraggedRow = oEvent.getParameter("target");
+			var oContextBinding = oDraggedRow.getBindingContext("listModel").getObject();
+			oDragSession.setComplexData("onListDragContext", oDraggedRow);
+		},
+	
 	});
 });
