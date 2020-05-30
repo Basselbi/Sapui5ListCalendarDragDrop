8813f9536ab77fb78b58b84498530b5a0e14873e
MrBass <belbizri@projexia.ca>
2020-05-30 01:50:58 -0400

    Adding bindings

--- webapp/Component.js
+++ webapp/Component.js
@@ -25,6 +25,8 @@ sap.ui.define([
 
 			// set the device model
 			this.setModel(models.createDeviceModel(), "device");
+			 
+			 
 			var products = [
 				{
 				  "ProductName": "Pineapple",
@@ -67,9 +69,157 @@ sap.ui.define([
 				  "Status": "A"
 				}
 			  ];
-			var oModel =  new sap.ui.model.json.JSONModel();
-			oModel.setData(products);
-			this.setModel(oModel ,"listModel");
+			var listModel =  new sap.ui.model.json.JSONModel();
+			var calendarModel =  new sap.ui.model.json.JSONModel();
+			listModel.setData(products);	 
+			calendarModel.setData({
+				startDate: new Date("2020", "10", "13", "8", "0"),
+				people: [
+					{
+						pic: "https://github.com/SAP/openui5/blob/master/src/sap.ui.documentation/test/sap/ui/documentation/sdk/images/John_Miller.png",
+						name: "John Miller",
+						role: "team member",
+						appointments: [
+							{
+								start: new Date("2020", "10", "13", "08", "00"),
+								end: new Date("2020", "10", "13", "09", "00"),
+								title: "Team sync",
+								info: "Canteen",
+								type: "Type07",
+								pic: "sap-icon://family-care"
+							},
+							{
+								start: new Date("2020", "10", "13", "09", "0"),
+								end: new Date("2020", "10", "13", "11", "0"),
+								title: "Morning Sync",
+								info: "I call you",
+								type: "Type01",
+								pic: "sap-icon://call"
+							},
+							{
+								start: new Date("2020", "10", "13", "10", "00"),
+								end: new Date("2020", "10", "13", "12", "00"),
+								title: "Sync Bill",
+								info: "Online",
+								type: "Type03"
+							},
+							{
+								start: new Date("2020", "10", "13", "10", "00"),
+								end: new Date("2020", "10", "13", "13", "00"),
+								title: "Check Flights",
+								info: "no room",
+								type: "Type09",
+								pic: "sap-icon://flight"
+							},
+							{
+								start: new Date("2020", "10", "13", "13", "00"),
+								end: new Date("2020", "10", "13", "14", "00"),
+								title: "Lunch",
+								info: "canteen",
+								type: "Type05",
+								pic: "sap-icon://private"
+							},
+							{
+								start: new Date("2020", "10", "13", "18", "00"),
+								end: new Date("2020", "10", "13", "20", "00"),
+								title: "Discussion of the plan",
+								info: "Online meeting",
+								type: "Type04"
+							},
+							{
+								start: new Date("2020", "10", "14", "03", "00"),
+								end: new Date("2020", "10", "14", "23", "00"),
+								title: "Deadline",
+								type: "Type05"
+							},
+							{
+								start: new Date("2020", "10", "14", "09", "00"),
+								end: new Date("2020", "10", "14", "14", "00"),
+								title: "Blocker",
+								info: "room 6",
+								type: "Type08"
+							},
+							{
+								start: new Date("2020", "10", "17", "09", "00"),
+								end: new Date("2020", "10", "17", "18", "00"),
+								title: "Boss Birthday",
+								type: "Type02"
+							},
+							{
+								start: new Date("2020", "10", "24", "09", "00"),
+								end: new Date("2020", "10", "24", "18", "00"),
+								title: "Urgent Planning",
+								type: "Type08"
+							},
+							{
+								start: new Date("2020", "10", "20", "01", "00"),
+								end: new Date("2020", "10", "20", "23", "00"),
+								title: "Planning",
+								type: "Type09"
+							}
+						]
+					},
+					{
+						pic: "test-resources/sap/ui/documentation/sdk/images/Donna_Moore.jpg",
+						name: "Donna Moore",
+						role: "team member",
+						appointments: [
+							{
+								start: new Date("2020", "10", "13", "08", "00"),
+								end: new Date("2020", "10", "13", "09", "26"),
+								title: "Team sync",
+								info: "Canteen",
+								type: "Type07",
+								pic: "sap-icon://family-care"
+							},
+							{
+								start: new Date("2020", "10", "13", "10", "00"),
+								end: new Date("2020", "10", "13", "12", "00"),
+								title: "Sync John",
+								info: "Online",
+								type: "Type03"
+							},
+							{
+								start: new Date("2020", "10", "13", "11", "00"),
+								end: new Date("2020", "10", "13", "12", "00"),
+								title: "Prep for planning",
+								info: "room 5",
+								type: "Type01",
+								pic: "sap-icon://family-care"
+							},
+							{
+								start: new Date("2020", "10", "13", "18", "00"),
+								end: new Date("2020", "10", "13", "20", "00"),
+								title: "Check Flights",
+								info: "no room",
+								type: "Type09",
+								pic: "sap-icon://flight"
+							},
+							{
+								start: new Date("2020", "10", "13", "18", "00"),
+								end: new Date("2020", "10", "13", "20", "00"),
+								title: "Discussion of the plan",
+								info: "Online meeting",
+								type: "Type04"
+							},
+							{
+								start: new Date("2020", "10", "20", "01", "00"),
+								end: new Date("2020", "10", "20", "23", "00"),
+								title: "Planning",
+								type: "Type09"
+							},
+							{
+								start: new Date("2018", "2", "20", "01", "00"),
+								end: new Date("2018", "2", "20", "23", "00"),
+								title: "Off",
+								type: "Type08"
+							}
+						]
+					}
+				]
+			});
+			this.setModel(calendarModel,"calendarModel");
+			this.setModel(listModel ,"listModel");
 		}
 	});
 });
